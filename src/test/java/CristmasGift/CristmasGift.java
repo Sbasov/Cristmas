081a0eb14b2eb31ce4928abbf2338278cf17572a
Stepan <Agent_77oo7@mail.ru>
2018-08-20 20:00:11 +0300

    Initial commit

---
+++ src/test/java/CristmasGift/CristmasGift.java
@@ -0,0 +1,26 @@
+package CristmasGift;
+
+import Sweets.Sweets;
+import Sweets.Cake;
+import Sweets.Candy;
+import Sweets.Chokolate;
+
+public class CristmasGift {
+    public static void main(String[] args) {
+
+        Cake cake = new Cake("Кекс",50,200,1);
+        Candy candy=new Candy("Аленка",20,150,2);
+        Chokolate chokolate=new Chokolate("Милка",25,400,3);
+        Sweets[] box = {cake,candy,chokolate};
+        for (Sweets somePar : box) {
+            System.out.println(somePar.toString());
+        }
+        //сумма подарка
+        int cena = cake.getPrice()+candy.getPrice()+chokolate.getPrice();
+        System.out.println(" Цена подарка = " + cena);
+
+        //Вес подарка
+        int ves=cake.getWes()+candy.getWes()+chokolate.getWes();
+        System.out.println(" Вес подарка = "+ves);
+    }
+}
---
+++ src/test/java/Sweets/Candy.java
@@ -0,0 +1,31 @@
+package Sweets;
+
+/**
+ * Created by User on 20.08.2018.
+ */
+public class Candy extends Sweets {
+
+    private int  unicIdentifiCandy; //поля класса
+
+    public Candy (String Name, int Wes, int Price, int unicIdentifiCandy)
+    {
+        super(Name,Wes,Price);
+        this.unicIdentifiCandy=unicIdentifiCandy;
+    }
+
+    public void setUnicIdentifiCandy(int unicIdentifiCandy)
+    {
+        this.unicIdentifiCandy=unicIdentifiCandy;
+    }
+    public int getUnicIdentifiCandy()
+    {
+        return unicIdentifiCandy;
+    }
+    @Override
+    public String toString()
+    {
+        return "Кексы  [ " +super.toString()+"; Уникальный номер = "+unicIdentifiCandy+" ] ";
+
+    }
+
+}
